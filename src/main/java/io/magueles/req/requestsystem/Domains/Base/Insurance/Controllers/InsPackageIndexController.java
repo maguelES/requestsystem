9434978d6ccc17c40e047c2c266ecf1f7092ef32
Mohd Amsyar <nerokyt2@gmail.com>
2023-11-26 15:54:07 +0800

    test: find using path variable

--- src/main/java/io/magueles/req/requestsystem/Domains/Base/Insurance/Controllers/InsPackageIndexController.java
+++ src/main/java/io/magueles/req/requestsystem/Domains/Base/Insurance/Controllers/InsPackageIndexController.java
@@ -1,20 +1,35 @@
 package io.magueles.req.requestsystem.Domains.Base.Insurance.Controllers;
 
 import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RequestMapping("/api/packages")
 @RestController
 public class InsPackageIndexController {
     
     @GetMapping()
     public ResponseEntity<Object> index() {
+        var res = new HashMap<String, String>();
+
+        res.put("message", "OK");
+        res.put("data", "1");
+
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> find(@PathVariable String id) {
+
+        log.info("Reacting to", id);
+
         var res = new HashMap<String, String>();
 
         res.put("message", "OK");
